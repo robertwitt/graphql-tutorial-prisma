4afafa3c55ebdcb3cce2167c8faba5eaef39b703
Robert Witt <robert.witt@zalando.de>
2019-05-01 17:09:51 +0200

    Use Prisma bindings to mutate data.

--- src/prisma.js
+++ src/prisma.js
@@ -5,5 +5,33 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{ id name posts { id title } }').then(data => console.log(JSON.stringify(data, undefined, 2)))
-prisma.query.comments(null, '{ id text author { id name } }').then(data => console.log(JSON.stringify(data, undefined, 2)))
+// prisma.query.users(null, '{ id name posts { id title } }').then(data => console.log(JSON.stringify(data, undefined, 2)))
+// prisma.query.comments(null, '{ id text author { id name } }').then(data => console.log(JSON.stringify(data, undefined, 2)))
+
+// prisma.mutation.createPost({
+//     data: {
+//         title: "GraphQL 101",
+//         body: "",
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: "cjv51wxt600dn07185zxbuusj"
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then(data => {
+//     console.log(data)
+//     return prisma.query.users(null, '{ id name posts { id title } }')
+// }).then(data => console.log(JSON.stringify(data, undefined, 2)))
+
+prisma.mutation.updatePost({
+    where: {
+        id: "cjv5cnd7a00io0718iaiypufv"
+    },
+    data: {
+        body: "This is how to get started with GraphQL ...",
+        published: true
+    }
+}, '{ id body published }')
+    .then(() => prisma.query.posts(null, '{ id title body published }'))
+    .then(data => console.log(JSON.stringify(data, undefined, 2)))
