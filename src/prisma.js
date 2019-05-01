b837b467a38206ec9fed740c0dda2f06ff15d5b6
Robert Witt <robert.witt@zalando.de>
2019-05-01 16:54:29 +0200

    Use Prisma bindings to query data.

--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,8 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'localhost:4466'
+    endpoint: 'http://localhost:4466'
 })
+
+prisma.query.users(null, '{ id name posts { id title } }').then(data => console.log(JSON.stringify(data, undefined, 2)))
+prisma.query.comments(null, '{ id text author { id name } }').then(data => console.log(JSON.stringify(data, undefined, 2)))
