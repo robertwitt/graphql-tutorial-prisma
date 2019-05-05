e9cc7ce8c65be1bab031ca7cb124bf3063ba9a33
Robert Witt <robert.witt@zalando.de>
2019-05-05 17:13:11 +0200

    Extract token generation into utility function.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import getUserId from '../utils/getUserId'
+import generateToken from '../utils/generateToken'
 
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
@@ -18,7 +19,7 @@ const Mutation = {
 
         return {
             user: user,
-            token: jwt.sign({ userId: user.id }, 'thisisasecret')
+            token: generateToken(user.id)
         }
     },
     async login(parent, args, { prisma }, info) {
@@ -38,7 +39,7 @@ const Mutation = {
 
         return {
             user: user,
-            token: jwt.sign({ userId: user.id }, 'thisisasecret')
+            token: generateToken(user.id)
         }
     },
     deleteUser(parent, args, { prisma, request }, info) {
