c9e162262aceea0885c786d84f77b5a79464f39c
Robert Witt <robert.witt@zalando.de>
2019-05-07 11:16:03 +0200

    Pull JWT secret out of code.

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
 
-const generateToken = userId => jwt.sign({ userId }, 'thisisasecret', { expiresIn: '7 days' })
+const generateToken = userId => jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7 days' })
 
 export { generateToken as default }
