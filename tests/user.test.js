69c15111846b1436a34a1df00db872c150d2e163
Robert Witt <robert.witt@zalando.de>
2019-05-09 15:04:49 +0200

    Test queries.

--- tests/user.test.js
+++ tests/user.test.js
@@ -68,3 +68,39 @@ test('Should create a new user', async () => {
     const userExists = await prisma.exists.User({ id: response.data.createUser.user.id })
     expect(userExists).toBe(true)
 })
+
+test('Should expose public author profiles', async () => {
+    const getUsers = gql`
+        query {
+            users {
+                id
+                name
+                email
+            }
+        }
+    `
+
+    const response = await client.query({ query: getUsers })
+
+    expect(response.data.users.length).toBe(1)
+    expect(response.data.users[0].email).toBe(null)
+    expect(response.data.users[0].name).toBe('Jen')
+})
+
+test('Should expose published posts', async () => {
+    const getPosts = gql`
+        query {
+            posts {
+                id
+                title
+                body
+                published
+            }
+        }
+    `
+
+    const response = await client.query({ query: getPosts })
+
+    expect(response.data.posts.length).toBe(1)
+    expect(response.data.posts[0].published).toBe(true)
+})
